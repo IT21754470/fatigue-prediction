040cc995aa6cc2c796edf45cfff9c7528f220a2c
IT21754470 <it21754470@my.sliit.lk>
2025-07-21 00:07:22 +0530

    file path changed

--- services/improvement_services.py
+++ services/improvement_services.py
@@ -3,20 +3,21 @@ import pickle
 
 def load_model():
     try:
-        # Adjust path to go up two levels to project root, then into models/
-        model_path = Path(__file__).resolve().parents[2] / 'models/swimming_improvement_models.pkl'
+        # model file is inside the same directory as this script
+        model_path = Path(__file__).resolve().parent / 'swimming_improvement_models.pkl'
         print("Trying to load model from:", model_path)
 
         if not model_path.exists():
             raise FileNotFoundError(f"Model file not found at: {model_path}")
-        
+
         with open(model_path, 'rb') as f:
             loaded_models = pickle.load(f)
-        
+
         return loaded_models
     except Exception as e:
         print(f"Error loading model: {str(e)}")
         raise
+
 
 
 def get_prediction_description(value):
